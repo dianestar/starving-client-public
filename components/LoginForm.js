63ab559df77c199c496299a8c93c78b92604a584
maaaaaaaaad <integral5137@gmail.com>
2022-04-09 16:03:37 +0900

    f

--- components/LoginForm.js
+++ components/LoginForm.js
@@ -2,11 +2,13 @@ import React from "react";
 import FormWrapper from "./FormWrapper";
 
 const LoginForm = () => {
-    return (
-        <FormWrapper title="로그인" text1="비밀번호 찾기" text2="회원 가입하기">
-            
-        </FormWrapper>
-    );
-}
+  return (
+    <FormWrapper
+      title="로그인"
+      text1="비밀번호 찾기"
+      text2="회원 가입하기"
+    ></FormWrapper>
+  );
+};
 
 export default LoginForm;
